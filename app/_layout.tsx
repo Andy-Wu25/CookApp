6de4755738fe0c9a7a650262f56682b7934debe1
Andy Wu <andywu2513@gmail.com>
2025-06-04 11:23:32 +0100

    navigation bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Stack } from "expo-router";
+import './globals.css';
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <>
+        {/* <StatusBar hidden={true} /> Hides the status bar for the entire app */}
+        <Stack>
+          <Stack.Screen
+              name="(tabs)"
+              options={{headerShown: false}} // Hides the header for the group tabs layout
+          />
+
+          <Stack.Screen
+              name="recipes/[id]"
+              options={{headerShown: false}}
+          />
+        </Stack>
+      </>
   );
 }
